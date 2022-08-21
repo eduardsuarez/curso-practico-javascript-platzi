ab6b520d36f17a5ee2573c4a9a3c2dd569bf9126
Eduard Suárez <eduardsb21@outlook.com>
2022-08-20 22:00:02 -0500

    Se cambian las imagenes de los productos

--- main.js
+++ main.js
@@ -17,6 +17,25 @@ menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
+let = `<div class="product-detail-close">
+<img src="./icons/icon_close.png" alt="close" />
+</div>
+<img
+src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+alt="bike"
+/>
+<div class="product-info">
+<p>$35,00</p>
+<p>Bike</p>
+<p>
+  With its functional and practical interior, this refrigerator also
+  fulfills a decorative function.
+</p>
+<button class="primary-button add-to-cart-button">
+  <img src="./icons/bt_add_to_cart.svg" alt="add to cart" />Add to cart
+</button>
+</div>`;
+
 
 
 function toggleDesktopMenu() {
@@ -59,6 +78,7 @@ function toggleCarritoAside() {
 }
 
 function openProductDetailAside(){
+
   shoppingCartContainer.classList.add('inactive');
 
   productDetailContainer.classList.remove('inactive');
@@ -69,6 +89,9 @@ function closeProductDetailAside(){
   productDetailContainer.classList.add('inactive');
 
 }
+
+
+
 const productList = [];
 productList.push({
   name: "Bike",
@@ -78,38 +101,38 @@ productList.push({
 });
 
 productList.push({
-  name: "Bike",
-  price: 120,
+  name: "Camera",
+  price: 230,
   image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600",
 });
 
 productList.push({
-  name: "Bike",
-  price: 120,
+  name: "NoteBook",
+  price: 520,
   image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://images.pexels.com/photos/4158/apple-iphone-smartphone-desk.jpg?auto=compress&cs=tinysrgb&w=600",
 });
 
 productList.push({
-  name: "Bike",
-  price: 120,
+  name: "Clock",
+  price: 140,
   image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg?auto=compress&cs=tinysrgb&w=600",
 });
 
 productList.push({
-  name: "Bike",
-  price: 120,
+  name: "Shoes",
+  price: 100,
   image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://images.pexels.com/photos/1537671/pexels-photo-1537671.jpeg?auto=compress&cs=tinysrgb&w=600",
 });
 
 productList.push({
-  name: "Bike",
-  price: 120,
+  name: "Keyboard",
+  price: 80,
   image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://images.pexels.com/photos/4383928/pexels-photo-4383928.jpeg?auto=compress&cs=tinysrgb&w=600",
 });
 
 function renderProducts(arr){
